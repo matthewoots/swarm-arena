2671f54c5f9ac6efe78df4504847d98bb07eadae
zhengtian <mazt1025@hotmail.com>
2022-12-26 11:48:48 +0800

    360 cloud since we will not yaw to the target before we plan and move

--- planner/plan_env/src/grid_map.cpp
+++ planner/plan_env/src/grid_map.cpp
@@ -885,7 +885,8 @@ void GridMap::publishMapInflate()
         for (double zd = lbz + mp_.resolution_ / 2; zd < ubz; zd += mp_.resolution_)
         {
           Eigen::Vector3d relative_dir = (Eigen::Vector3d(xd, yd, zd) - md_.camera_pos_);
-          if (heading.dot(relative_dir.normalized()) > 0.5)
+          // if (heading.dot(relative_dir.normalized()) > 0.5)
+          if(1)
           {
             if (md_.occupancy_buffer_inflate_[globalIdx2InfBufIdx(pos2GlobalIdx(Eigen::Vector3d(xd, yd, zd)))])
               cloud.push_back(pcl::PointXYZ(xd, yd, zd));
